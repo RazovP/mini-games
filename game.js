58e4da1dfb8b67edf8db885033020aff144bf35b
RazovP <baxikmc@gmail.com>
2024-12-16 22:16:49 +0300

    Непростая арифметика)

--- game.js
+++ game.js
@@ -15,8 +15,48 @@ function guessGame() {
         } else {
             userNumber = prompt(`Загаданное число меньше ${userNumber}, попробуй еще раз!`);
         }
-        console.log(userNumber);
+
     }
 
     alert("Молодец, ты угадал загаданное число!!!!")
+}
+
+// Игра простая арифметика
+
+function arifm() {
+    const firstNumber = Math.ceil(Math.random() * 20);
+    const secondNumber = Math.ceil(Math.random() * 20);
+    const operations = ['+', '-', '*', '/'];
+    const randomOperation = Math.ceil(Math.random() * 4);
+    let result
+    switch (randomOperation) {
+        case 1:
+            result = firstNumber + secondNumber;
+            break;
+        case 2:
+            result = firstNumber - secondNumber;
+            break;
+        case 3:
+            result = firstNumber * secondNumber;
+            break;
+        case 4:
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            break;
+
+    }
+    result = Math.round(result * 10) / 10;
+    console.log(result);
+    userNumber = prompt(`Введите результат арифметической операции: ${firstNumber} ${operations[randomOperation - 1]} ${secondNumber}. Если результат не целое число, округлите его до первого знака после запятой.`);
+
+
+    while (userNumber != result) {
+        if (userNumber === null) {
+            return;
+        }
+        userNumber = prompt(`Неверно, попробуйте еще раз! Введите результат арифметической операции: ${firstNumber} ${operations[randomOperation - 1]} ${secondNumber}`);
+    }
+    alert(`Молодец!!!`)
+
 }
