e04006d75b6f518478676fae332dbe2f381b854e
RazovP <baxikmc@gmail.com>
2025-01-23 20:08:49 +0300

    пупупу

--- hw-8.js
+++ hw-8.js
@@ -1,12 +1,12 @@
 // Задание 1
 
 let people = [
-	{ name: 'Глеб', age: 29 },
-	{ name: 'Анна', age: 17 },
-	{ name: 'Олег', age: 7 },
-	{ name: 'Оксана', age: 47 }
- ];
- 
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+];
+
 
 console.log(people.sort((a, b) => a.age - b.age));
 
@@ -21,26 +21,26 @@ function isMale(person) {
 }
 
 function filter(array, ruleFunction) {
-    const result = []; 
+    const result = [];
     for (let i = 0; i < array.length; i++) {
-        if (ruleFunction(array[i])) { 
+        if (ruleFunction(array[i])) {
             result.push(array[i]);
         }
     }
-    return result; 
+    return result;
 }
 
-console.log(filter([3, -4, 1, 9], isPositive)); 
+console.log(filter([3, -4, 1, 9], isPositive));
 
 people = [
-    {name: 'Глеб', gender: 'male'},
-    {name: 'Анна', gender: 'female'},
-    {name: 'Олег', gender: 'male'},
-    {name: 'Оксана', gender: 'female'}
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
 ];
 
-console.log(filter(people, isMale)); 
- 
+console.log(filter(people, isMale));
+
 // Задание 3
 
 function logCurrentDate() {
@@ -61,7 +61,7 @@ function delayForSecond4(callback) {
     setTimeout(callback, 4000);
 }
 
-delayForSecond(function () {
+delayForSecond4(function () {
     console.log('Привет, Глеб!');
 });
 
@@ -70,11 +70,11 @@ delayForSecond(function () {
 function delayForSecond(cb) {
     setTimeout(() => {
         console.log('Прошла одна секунда');
-        if(cb) {  cb(); }
+        if (cb) { cb(); }
     }, 1000)
 }
 
-function sayHi (name) {
+function sayHi(name) {
     console.log(`Привет, ${name}!`);
 }
 
